2582bdf351db45dde13c4499cca38d01cad6d182
Nicholas McMaster <nmcmaster@gmail.com>
2020-12-02 12:54:15 -0500

    memoized one dependency for optimization

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -1,11 +1,11 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 
 export const useGameStatus = rowsCleared => {
   const [score, setScore] = useState(0);
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-  const linePoints = [40, 100, 300, 1200];
+  const linePoints = useMemo(() => [40, 100, 300, 1200], []);
 
   const calcScore = useCallback(() => {
     // if we have score
